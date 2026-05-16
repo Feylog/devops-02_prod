f50c439e2098f4bc62bf686bd2ad218c72aa21fc
Case <Case@devops-homework.local>
2026-05-16 15:50:48 +0000

    feat(ansible+terraform): add kubeadm role and NAT gateway for workers
    - New ansible role 'kubeadm' installs containerd + kubelet/kubeadm/kubectl
    - Configures kernel modules, sysctl, swap-off, SystemdCgroup
    - New cluster.yml playbook targets kube_cluster group (control_plane + workers)
    - Terraform network module adds NAT gateway + route table for internal-only VMs

--- infra/terraform/modules/network/main.tf
+++ infra/terraform/modules/network/main.tf
@@ -2,9 +2,25 @@ resource "yandex_vpc_network" "net" {
   name = "${var.name_prefix}-net"
 }
 
+resource "yandex_vpc_gateway" "nat" {
+  name = "${var.name_prefix}-nat-gw"
+  shared_egress_gateway {}
+}
+
+resource "yandex_vpc_route_table" "rt" {
+  name       = "${var.name_prefix}-rt"
+  network_id = yandex_vpc_network.net.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id         = yandex_vpc_gateway.nat.id
+  }
+}
+
 resource "yandex_vpc_subnet" "subnet" {
   name           = "${var.name_prefix}-subnet-a"
   zone           = var.zone
   network_id     = yandex_vpc_network.net.id
   v4_cidr_blocks = ["10.10.0.0/24"]
+  route_table_id = yandex_vpc_route_table.rt.id
 }
